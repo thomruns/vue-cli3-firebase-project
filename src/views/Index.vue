4bf1e86aa43d174834c94ec7f1d550d0a3ae26be
Thom David <thomdavid74@gmail.com>
2019-05-06 13:58:38 -0700

    delete dummy item functionality

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="index container">
-      <div class="card card-panel blue lighten-5" v-for="movie in movies" :key="movie.id">
+      <div class="card card-panel grey lighten-4" v-for="movie in movies" :key="movie.id">
         <div class="card-content">
+          <i class="material-icons delete" @click="deleteMovie(movie.id)">delete</i>
           <h2 class="blue-text text-darken-3">{{ movie.title }}</h2>
           <ul class="stars">
             <li v-for="(star, index) in movie.stars" :key="index">
@@ -27,11 +28,20 @@ export default {
         { title: 'Cannablanca', slug: 'cannablanca', stars: ['Humphrey Brogart', 'Ingrid Bergdorf', 'Pill Henrood', 'Cloud Rains'], id: 2
         },
         { title: 'The Woolman', slug: 'the-woolman', stars: ['Len Chaney Jr', 'Cloud Rains', 'Evilyn Akers'], id: 3
+        },
+        { title: 'Darcula', slug: 'darcula', stars: ['Belly Lugosi', 'Dwight Fried', 'Edward Slong'], id: 4
         }
       ]
     }
   },
   components: {
+  },
+  methods: {
+    deleteMovie(id) {
+      this.movies = this.movies.filter(movie => {
+        return movie.id != id
+      })
+    }
   }
 }
 </script>
@@ -41,7 +51,7 @@ export default {
   display: grid;
   grid-template-columns: 1fr 1fr 1fr;
   grid-gap: 30px;
-  margin-top: 40px;
+  margin-top: 30px;
 }
 .index h2 {
   font-size: 1.8em;
@@ -56,6 +66,14 @@ export default {
 }
 .star-name{
   font-size: 11px;
+}
+.index .delete {
+  position: absolute;
+  top: 4px;
+  right: 4px;
+  cursor: pointer;
+  color: #aaa;
+  font-size: 1.4em;
 }
 </style>
 
