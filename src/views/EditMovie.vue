516aa5e0179c4a1c0ccdbeb7569a2f828f452a85
Thom David <thomdavid74@gmail.com>
2019-05-08 14:37:53 -0700

    Get database data for editing

--- src/views/EditMovie.vue
+++ src/views/EditMovie.vue
@@ -1,17 +1,27 @@
 <template>
-  <div class="edit-movie container">
-    <h2>Edit {{ this.$route.params.movie_slug }}</h2>
+  <div v-if="movie" class="edit-movie container">
+    <h2>Edit {{ movie.title }}</h2>
   </div>
 </template>
 
 <script>
 import db from '@/firebase/init.js'
+
 export default {
   name: 'EditMovie',
   data() {
     return {
-
+      movie: null
     }
+  },
+  created() {
+   let ref = db.collection('movies').where('slug', '==', this.$route.params.movie_slug)
+   ref.get().then((snapshot) => {
+     snapshot.forEach(doc => {
+       this.movie = doc.data()
+       this.movie.id = doc.id
+     })
+   })
   }
 }
 </script>
